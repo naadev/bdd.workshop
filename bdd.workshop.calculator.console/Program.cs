b164827aeb671654918d736f2bc9729955904896
Nicolas Afonso <SESA219774@se.com>
2021-10-28 18:57:11 +0200

    NOTE: Divide fails because it divides two integers. It will be fixed on next iteration

---
+++ bdd.workshop.calculator.console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace bdd.workshop.calculator.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter operation: separate number and operator by blank space, example: 3 + 4");
+            var command = Console.ReadLine();
+            Operator.CommandManager(command, out int a, out int b, out string operation);
+            switch (operation)
+            {
+                case ("+"):
+                    Console.WriteLine(Operator.Add(a, b));
+                    break;
+                case ("-"):
+                    Console.WriteLine(Operator.Substract(a, b));
+                    break;
+                case ("x"):
+                    Console.WriteLine(Operator.Multiply(a, b));
+                    break;
+                case ("/"):
+                    Console.WriteLine(Operator.Divide(a, b));
+                    break;
+                default:
+                    Console.WriteLine("Possible operators: +,-,x,/: i.e.: 3 + 4");
+                    break;
+            }
+        }
+    }
+}
---
+++ bdd.workshop.calculator/Operator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace bdd.workshop.calculator
+{
+    public static class Operator
+    {
+        public static void CommandManager(string command,out int a,out int b, out string operation)
+        {
+
+            var parts = command.Split(" ");
+            if (parts.Count() != 3)
+            {
+                throw new InvalidOperationException("Ensure you're entering two integers and the operator split by blank space, in example, 3 + 4");
+            }
+            if (!int.TryParse(parts[0], out a))
+            {
+                throw new InvalidOperationException("First entity must be an integer");
+            }
+            if (!int.TryParse(parts[2], out b))
+            {
+                throw new InvalidOperationException("Last entity must be an integer");
+            }
+            operation = parts[1];
+        }
+        public static int Add(int a, int b) => a + b;
+
+        public static int Multiply(int a, int b) => a * b;
+
+        public static double Divide(int a, int b) => a / b;
+
+        public static int Substract(int a, int b) => a - b;
+
+    }
+}
