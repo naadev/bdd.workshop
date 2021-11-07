1a3fdca24052c0d7cf3e229fa9d6c358d7ec8da6
Nicolas Afonso <SESA219774@se.com>
2021-11-07 19:08:14 +0100

    Using Feature files on Functional tests

---
+++ bdd.workshop.calculator.tests.selenium/steps/Calculator.cs
@@ -0,0 +1,101 @@
+﻿using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace bdd.workshop.calculator.tests.selenium.steps
+{
+    [Binding]
+    public sealed class Calculator : WebBrowser
+    {
+        private double EvaluateOperation(int a, int b, string operation)
+        {
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            var aXpath = "//input[@id='A_TheNumber']";
+            var bXpath = "//input[@id='B_TheNumber']";
+            var cmdXpath = "//input[@id='Command']";
+            var submitButton = "//input[@type='submit']";
+            Driver.Url = "https://bdd-workshop-the-calculator.azurewebsites.net/Calculator";
+            var inputA = FindElement(aXpath, wait);
+            var inputCmd = FindElement(cmdXpath, wait);
+            var inputB = FindElement(bXpath, wait);
+            var button = FindElement(submitButton, wait);
+            inputA.SendKeys(a.ToString());
+            inputCmd.SendKeys(operation);
+            inputB.SendKeys(b.ToString());
+            button.Click();
+            var theResult = "//td[@id='theResult']";
+            var outputResultString = FindElement(theResult, wait).Text;
+            return double.Parse(outputResultString);
+        }
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public Calculator(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int firstNumber)
+        {
+            _scenarioContext.Add("FirstNumber", firstNumber);
+        }
+
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int secondNumber)
+        {
+            _scenarioContext.Add("SecondNumber", secondNumber);
+        }
+
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            var firstNumber = _scenarioContext.Get<int>("FirstNumber");
+            var secondNumber = _scenarioContext.Get<int>("SecondNumber");
+            _scenarioContext.Add("Result", EvaluateOperation(firstNumber, secondNumber, "+"));
+        }
+
+        [Then(@"the result should be (.*)")]
+        [Then(@"the result shall be (.*)")]
+        public void ThenTheResultShouldBe(double result)
+        {
+            Assert.True(result == _scenarioContext.Get<double>("Result"));
+        }
+
+        [When(@"I substract first number to second number")]
+        public void WhenISubstractFirstNumberToSecondNumber()
+        {
+            var firstNumber = _scenarioContext.Get<int>("FirstNumber");
+            var secondNumber = _scenarioContext.Get<int>("SecondNumber");
+            _scenarioContext.Add("Result",EvaluateOperation(firstNumber, secondNumber, "-"));
+        }
+
+        [When(@"I multiply both numbers")]
+        public void WhenIMultiplyBothNumbers()
+        {
+            var firstNumber = _scenarioContext.Get<int>("FirstNumber");
+            var secondNumber = _scenarioContext.Get<int>("SecondNumber");
+            _scenarioContext.Add("Result", EvaluateOperation(firstNumber, secondNumber, "x"));
+        }
+
+        [When(@"I divide first number by second number")]
+        public void WhenIDivideFirstNumberBySecondNumber()
+        {
+            var firstNumber = _scenarioContext.Get<int>("FirstNumber");
+            var secondNumber = _scenarioContext.Get<int>("SecondNumber");
+            _scenarioContext.Add("Result", EvaluateOperation(firstNumber, secondNumber, "/"));
+        }
+
+
+        [Then(@"the result is (.*)")]
+        public void ThenTheResultIs(double result)
+        {
+            Assert.True(result == _scenarioContext.Get<double>("Result"));
+        }
+
+    }
+}
---
+++ bdd.workshop.calculator.tests.selenium/steps/PrimeNumberSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace bdd.workshop.calculator.tests.selenium.steps
+{
+    [Binding]
+    public sealed class PrimeNumberSteps : WebBrowser
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public PrimeNumberSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+        [When(@"number (.*) is checked")]
+        public void WhenNumberIsChecked(int number)
+        {
+
+        }
+        
+        [Then(@"the answer to know whether is prime or not is (.*)")]
+        public void ThenTheAnswerToKnowWhetherIsPrimeOrNotIsUnknown(string primeNumberInfo)
+        {
+        }
+    }
+}
