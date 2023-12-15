ec3986287c5adce8ff36bf15cd09fb34e4ced2a5
Dmitry Ivanov <d.k.ivanov@live.com>
2023-12-15 20:18:09 +0100

    Add UI MVC functionality for Square Root calculation

---
+++ bdd.workshop.calculator.web/Controllers/SquareRootController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bdd.workshop.calculator.web.Controllers
+{
+    public class SquareRootController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SquareRoot(Models.SquareRoot squareRooter)
+        {
+            ViewData["number"] = squareRooter.Number.TheNumber;
+            ViewData["result"] = Operator.SquareRoot(squareRooter.Number.TheNumber);
+            return View();
+        }
+    }
+}
---
+++ bdd.workshop.calculator.web/Models/SquareRoot.cs
@@ -0,0 +1,7 @@
+﻿namespace bdd.workshop.calculator.web.Models
+{
+    public class SquareRoot
+    {
+        public Number Number { get; set; } = new Number();
+    }
+}
