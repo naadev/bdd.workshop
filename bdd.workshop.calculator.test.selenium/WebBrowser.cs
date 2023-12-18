589ae7863b88cb713831be716d996d127084bb15
Hector Betancourt <hbetancourt@correo.ugr.es>
2023-12-18 09:19:14 -0500

    UpdateChromePath

--- bdd.workshop.calculator.test.selenium/WebBrowser.cs
+++ bdd.workshop.calculator.test.selenium/WebBrowser.cs
@@ -11,7 +11,7 @@ namespace bdd.workshop.calculator.test.selenium
 {
     public abstract class WebBrowser : IDisposable
     {
-        protected IWebDriver Driver { get; set; } = new ChromeDriver("C:\\ChromeDriver\\");
+        protected IWebDriver Driver { get; set; } = new ChromeDriver("/usr/bin/");
 
         public void Dispose()
         {
