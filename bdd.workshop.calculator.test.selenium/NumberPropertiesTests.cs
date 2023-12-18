b7a06741670efb8af8f094133e54218bb48b3049
jcarlosorte <jcarlosorte@ugr.es>
2023-12-18 03:07:43 +0100

    Refact v0.2: 1.Se crearon constantes para los XPaths y la URL para facilitar el mantenimiento. 2. Se creó un método NavigateToCalculatorPage para la navegación a la página del calculador. 3. Se creó un método EnterNumberAndSubmit para ingresar el número y hacer clic en el botón de envío. 4. Se creó un método VerifyNumberProperties para verificar las propiedades del número después de enviarlo. 5. Se utilizó una propiedad Wait para obtener la instancia de WebDriverWait de manera más limpia. 6. Se simplificó el método PrimeNumberTest para hacerlo más claro y centrado en la lógica de prueba.

--- bdd.workshop.calculator.test.selenium/NumberPropertiesTests.cs
+++ bdd.workshop.calculator.test.selenium/NumberPropertiesTests.cs
@@ -12,6 +12,15 @@ namespace bdd.workshop.calculator.test.selenium
 {
     public class NumberPropertiesTests : WebBrowser
     {
+        private const string CalculatorUrl = "https://bdd-workshop-the-calculator.azurewebsites.net/NumberProperties";
+        private const string NumberXPath = @"//input[@id='TheNumber']";
+        private const string SubmitButtonXPath = "//input[@type='submit']";
+        private const string IsPrimeNumberXPath = "//td[@id='isPrimeNumber']";
+        private const string IsNotPrimeNumberXPath = "//td[@id='IsNotPrimeNumber']";
+        private const string IsUndefinedXPath = "//td[@id='IsUndefined']";
+
+        private WebDriverWait Wait => new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+
         [Theory(DisplayName = "Number Properties Prime Number Theory")]
         [Trait("TestType", "Functional Theories")]
         [InlineData(13, true, false, false)]
@@ -20,22 +29,33 @@ namespace bdd.workshop.calculator.test.selenium
         [InlineData(8, false, true, false)]
         [InlineData(369, false, true, false)]
         [InlineData(0, false, false, true)]
-        public void PrimeNumberTest(int number,bool isPrime,bool isNotPrime, bool isUndefined)
+        public void PrimeNumberTest(int number, bool isPrime, bool isNotPrime, bool isUndefined)
         {
-            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
-            var numberXPath = @"//input[@id='TheNumber']";
-            var submitButton = "//input[@type='submit']";
-            Driver.Url = "https://bdd-workshop-the-calculator.azurewebsites.net/NumberProperties";
-            var inputA = FindElement(numberXPath, wait);
-            var button = FindElement(submitButton, wait);
-            inputA.SendKeys(number.ToString());
-            button.Click();
-            var isPrimeNumberXPath = "//td[@id='isPrimeNumber']";
-            var isNotPrimeNumberXPath = "//td[@id='IsNotPrimeNumber']";
-            var isUndefinedXPath = "//td[@id='IsUndefined']";
-            var isPrimeNumberMark = FindElement(isPrimeNumberXPath, wait).Text == "X";
-            var isNotPrimeNumberMark = FindElement(isNotPrimeNumberXPath, wait).Text == "X";
-            var isUndefinedMark = FindElement(isUndefinedXPath, wait).Text == "X";
+            NavigateToCalculatorPage();
+            EnterNumberAndSubmit(number);
+            VerifyNumberProperties(isPrime, isNotPrime, isUndefined);
+        }
+
+        private void NavigateToCalculatorPage()
+        {
+            Driver.Url = CalculatorUrl;
+        }
+
+        private void EnterNumberAndSubmit(int number)
+        {
+            var wait = Wait;
+            var inputNumber = FindElement(NumberXPath, wait);
+            var submitButton = FindElement(SubmitButtonXPath, wait);
+            inputNumber.SendKeys(number.ToString());
+            submitButton.Click();
+        }
+
+        private void VerifyNumberProperties(bool isPrime, bool isNotPrime, bool isUndefined)
+        {
+            var wait = Wait;
+            var isPrimeNumberMark = FindElement(IsPrimeNumberXPath, wait).Text == "X";
+            var isNotPrimeNumberMark = FindElement(IsNotPrimeNumberXPath, wait).Text == "X";
+            var isUndefinedMark = FindElement(IsUndefinedXPath, wait).Text == "X";
             Assert.True(isPrime == isPrimeNumberMark);
             Assert.True(isNotPrime == isNotPrimeNumberMark);
             Assert.True(isUndefined == isUndefinedMark);
