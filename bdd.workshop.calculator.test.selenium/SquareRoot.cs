08b2aefcd89af556aed66061a37a6876a84afc86
Efrain Espadero <efrain.espadero@constructconnect.com>
2023-12-19 18:55:25 -0400

    Adding Automated Test

---
+++ bdd.workshop.calculator.test.selenium/SquareRoot.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace bdd.workshop.calculator.test.selenium
+{
+    public class SquareRoot: WebBrowser
+    {
+        [Theory(DisplayName = "Square Root Theory")]
+        [Trait("TestType", "Functional Theories")]
+        [InlineData(0, 0)]
+        [InlineData(4, 2)]
+        [InlineData(9, 3)]
+        [InlineData(16, 4)]
+        public void SquareRooTest(int number, double result)
+        {
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            var numberXPath = @"//input[@id='TheNumber']";
+            var submitButton = "//input[@type='submit']";
+            Driver.Url = "http://localhost:4234/SquareRoot";
+            var inputA = FindElement(numberXPath, wait);
+            var button = FindElement(submitButton, wait);
+            inputA.SendKeys(number.ToString());
+            button.Click();
+            var outputResult = "//td[@id='theResult']";
+            var outputResultString = FindElement(outputResult, wait).Text;
+            double.TryParse(outputResultString, out double doubleOutPutResult);
+            Assert.Equal(result, doubleOutPutResult);
+        }
+    }
+}
