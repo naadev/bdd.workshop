e4c75047ab35a9b23253771a876f7f77f989a2b4
Hector Betancourt <hbetancourt@correo.ugr.es>
2023-12-19 11:33:43 -0500

    ChromeDriverUpdate
    ChromeDriverUpdate

--- bdd.workshop.calculator.test.selenium/WebBrowser.cs
+++ bdd.workshop.calculator.test.selenium/WebBrowser.cs
@@ -11,7 +11,25 @@ namespace bdd.workshop.calculator.test.selenium
 {
     public abstract class WebBrowser : IDisposable
     {
-        protected IWebDriver Driver { get; set; } = new ChromeDriver("C:\\ChromeDriver\\");
+        public static string Route()
+        {
+            string res;
+            string os = Environment.OSVersion.Platform.ToString();
+            if (os.StartsWith("Win"))
+            {
+                res = "C:\\ChromeDriver\\";
+            }
+            else if (os.StartsWith("Linux"))
+            {
+                res = "/usr/bin/";
+            }
+            else
+            {
+                throw new NotSupportedException("Unsupported operating system.");
+            }
+            return res;
+        }
+        protected IWebDriver Driver { get; set; } = new ChromeDriver(Route());
         public void Dispose()
         {
             Driver.Close();
