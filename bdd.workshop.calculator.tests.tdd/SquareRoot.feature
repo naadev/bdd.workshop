9bd65c90f9cc5a6c9b0a8a282817345178bb4a29
Efrain Espadero <efrain.espadero@constructconnect.com>
2023-12-19 20:06:09 -0400

    Adding test using cucumber

---
+++ bdd.workshop.calculator.tests.tdd/SquareRoot.feature
@@ -0,0 +1,25 @@
+﻿Feature: SquareRoot
+	As Martha (the customer)
+	I want to execute binary operations
+	So I can save my time
+
+
+Scenario: Square Root of 0
+	Given the number 0
+	When get the square root
+	Then the result should be 0.0
+
+Scenario: Square Root of 4
+	Given the number 4
+	When get the square root
+	Then the result should be 2.0
+
+Scenario: Square Root of 9
+	Given the number 9
+	When get the square root
+	Then the result should be 3.0
+
+Scenario: Square Root of 16
+	Given the number 16
+	When get the square root
+	Then the result should be 4.0
---
+++ bdd.workshop.calculator.tests.tdd/steps/SquareRootSteps.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace bdd.workshop.calculator.tests.tdd.steps
+{
+    [Binding]
+    public class SquareRootSteps
+    {
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public SquareRootSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"the number (.*)")]
+        public void GivenTheNumber(int number)
+        {
+            _scenarioContext.Add("Number", number);
+        }
+
+        [When(@"get the square root")]
+        public void WhenGetTheSquareRoot()
+        {
+            var number = _scenarioContext.Get<int>("Number");
+            _scenarioContext.Add("Result", Operator.SquareRoot(number));
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(double result)
+        {
+            Assert.True(result == _scenarioContext.Get<double>("Result"));
+        }
+    }
+}
